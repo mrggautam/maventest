cc9e9819470316d1cc3c58c9a949cc0820dc72e8
mrggautam <mrg.gautam@gmail.com>
2020-08-19 01:55:11 +0530

    initial commit

---
+++ src/test/java/com/example/maventest/MaventestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.maventest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaventestApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
